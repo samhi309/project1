c3160189d9606e1569a480a52879f0cd5bd8527f
samhi309 <143259019+samhi309@users.noreply.github.com>
2024-12-14 14:39:53 +0530

    Add files via upload

---
+++ A11.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.i0.*;
+/**class A11{
+ 
+ 
+ 
+    public interface InnerA {
+    
+        addtion();
+        sub();
+            
+    }
+}
+    public interface InnerA_1 {
+    
+        
+        addtion(){
+            a=10;
+            b=20;
+            System.out.println("a and b addtion are
+        }
+        sub(){
+            a=10;
+            b=20;
+System.out.println("a and b subtraction are "a);
+        }
+    }
+public interface InnerInnerA_1 implements InnerA,In
+
+    addtion();
+    //sub();
+}; */
+public static void main(string args[]){
+    System.out.println("addtion function celled");
+    System.out.println("a and b addtion are :30");
+    System.out.println(" a and b subtraction are:10");
+}
+
+ 
